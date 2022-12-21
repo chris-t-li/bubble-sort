1fba07f6ff50a687381bb6f1732a677b8ed01842
Chris Li <chris.lty07@gmail.com>
2022-12-21 15:06:10 -0500

    JavaScript solution

--- javascript/bubble_sort.js
+++ javascript/bubble_sort.js
@@ -1,5 +1,19 @@
 function bubbleSort(arr) {
   // type your code here
+  // iterate over the array arr
+  // if arr[i] > arr[i+1] swap them, otherwise, keep going
+  // when there are no swaps left, return the array
+  let i = 0;
+
+  while (i < arr.length - 1) {
+    if (arr[i] > arr[i + 1]) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+      i = 0;
+    } else {
+      i += 1;
+    }
+  }
+  return arr;
 }
 
 if (require.main === module) {
@@ -7,7 +21,8 @@ if (require.main === module) {
   console.log("Expecting: [1, 2, 3, 4]");
   console.log("=>", bubbleSort([3, 2, 1, 4]));
 
-  console.log("");
+  console.log("Expecting: [-2, 1, 2, 3, 4, 6, 10]");
+  console.log("=>", bubbleSort([3, 2, 1, 4, 6, 10, -2]));
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", bubbleSort([1, 2, 3]));
